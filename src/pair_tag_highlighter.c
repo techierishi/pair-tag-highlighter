bdc94b245d3974f2c8baee0442480d693ae3b1a6
Volodymyr Kononenko <vm@kononenko.ws>
2013-03-31 16:22:01 +0300

    Implemented detecting tag under cursor and highlighting it.
    Plugin is able to detect, that cursor is inside a tag,
    and highlight it. Matching tag search and highlighting is
    to be implemented.

--- src/pair_tag_highlighter.c
+++ src/pair_tag_highlighter.c
@@ -7,20 +7,80 @@ GeanyPlugin     *geany_plugin;
 GeanyData       *geany_data;
 GeanyFunctions  *geany_functions;
 
+ScintillaObject *sci;
+
 PLUGIN_VERSION_CHECK(211)
 
 PLUGIN_SET_INFO("Pair Tag Highlighter", "Finds and highlights matching \
                 opening/closing HTML tag", "1.0", 
                 "Volodymyr Kononenko <vm@kononenko.ws>");
 
-void run_tag_highlighter(ScintillaObject *sci)
+/* searches openging tag brace to the left: from the current cursor
+ * position to the start of the line. */
+gint findOpeningBrace(gint position, gint lineStart)
+{
+    gint openingBrace = -1;
+    gint pos;
+    for(pos=position; pos>=lineStart; pos--)
+    {
+        gchar charAtCurPosition = sci_get_char_at(sci, pos-1);
+        if ('<' == charAtCurPosition)
+        {
+            openingBrace = pos;
+            break;
+        }
+    }
+    /* if opening brace is not found in the line, we're not inside tag */
+    if (-1 != openingBrace)
+        openingBrace = pos;
+
+    return openingBrace;
+}
+
+/* searches closing tag brace to the right: from the current cursor
+ * position to the end of the line. */
+gint findClosingBrace(gint position, gint lineEnd)
+{
+    gint closingBrace = -1;
+    gint pos;
+    for(pos=position; pos<=lineEnd; pos++)
+    {
+        gchar charAtCurPosition = sci_get_char_at(sci, pos);
+        if ('>' == charAtCurPosition)
+        {
+            closingBrace = pos;
+            break;
+        }
+    }
+    /* if closing brace is not found in the line, we're not inside tag */
+    if (-1 != closingBrace)
+        closingBrace = pos;
+
+    return closingBrace;
+}
+
+void highlight_tag(gint openingBrace, gint closingBrace)
+{
+    scintilla_send_message(sci, SCI_STARTSTYLING, openingBrace-1, 0x1f);
+    scintilla_send_message(sci, SCI_SETSTYLING, closingBrace-openingBrace+2, 36);
+    scintilla_send_message(sci, SCI_STYLESETBOLD, closingBrace-openingBrace+2, 1);
+}
+
+void run_tag_highlighter()
 {
     gint position = sci_get_current_position(sci);
+    gint lineNumber = sci_get_current_line(sci);
+    gint lineStart = sci_get_position_from_line(sci, lineNumber);
+    gint lineEnd = sci_get_line_end_position(sci, lineNumber);
+    gint openingBrace = findOpeningBrace(position, lineStart);
+    gint closingBrace = findClosingBrace(position, lineEnd);
 
-    /* test styling */
-    scintilla_send_message(sci, SCI_STARTSTYLING, position, 0x1f);
-    scintilla_send_message(sci, SCI_SETSTYLING, 3, 36);
-    scintilla_send_message(sci, SCI_STYLESETBOLD, 3, 1);
+    if (-1 == openingBrace || -1 == closingBrace)
+        return;
+
+    /* Highlight current tag. Matching tag will be highlighted from
+     * findMatchingTag() functiong */
+    highlight_tag(openingBrace, closingBrace);
 }
 
 /* Notification handler for editor-notify */
@@ -29,7 +89,9 @@ static gboolean on_editor_notify(GObject *obj, GeanyEditor *editor,
 {
     gint lexer;
 
-    lexer = sci_get_lexer(editor->sci);
+    /* setting global sci variable to be available in other functions */
+    sci = editor->sci;
+    lexer = sci_get_lexer(sci);
     if (lexer != SCLEX_HTML && lexer != SCLEX_XML)
     {
         return FALSE;
@@ -39,7 +101,7 @@ static gboolean on_editor_notify(GObject *obj, GeanyEditor *editor,
     switch (nt->nmhdr.code)
     {
         case SCN_UPDATEUI:
-            run_tag_highlighter(editor->sci);
+            run_tag_highlighter();
             break;
     }
 
